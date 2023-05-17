e922cda235c644cda716a3c74c6b0ba7dad24faf
nickbelwin <belwalkarsarvesh@gmail.com>
2023-05-17 22:47:23 +0530

    FirstCommit

--- Tutorial1.js
+++ Tutorial1.js
@@ -1,1 +1,35 @@
 console.log("Hey I'm Sarvesh");
+
+// chrome developer tools
+
+//browser developer tools
+
+// variable
+
+// for storing the data
+// whole the world all the website, software everything is nothing without the
+//data
+
+// text , number  , array , true/false
+
+// java  => diff , diff data tyep for variable
+
+// it is dynamic type conversion language
+//it is not static typed language => Java
+
+//In Javascript we have three keyword which is used for defining or
+// creating a variable => var, let, const
+
+//var => from initial before 17 june 2015
+
+//let and const came on ecmascript =>6 => es6
+
+//var 
+// in javascript anything inside the single quoted , double quoted and  ``(back-tick)
+var firstName="Sarvesh";
+var lastName='Belwalkar';
+var middleNmae=`Balkrishna`
+console.log(firstName, middleNmae, lastName);
+
+// typeof keyword will tell you what is the data type
+console.log(typeof firstName, typeof middleNmae, typeof lastName);
